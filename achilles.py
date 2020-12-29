659c6200c0f0a75fa4202c12591ee6ee748e767d
Jeff MacDonald <jam@zoidtechnologies.com>
2020-12-28 22:42:33 -0500

    achilles/achilles.py: started work on main menu; used argparse module to set up cli args; used new bbsengine.buildargdatabasegroup().

--- achilles.py
+++ achilles.py
@@ -6,11 +6,10 @@ import os
 import pwd
 import copy
 import tempfile
+import argparse
 
-from optparse import OptionParser
-
+import getdate
 import ttyio4 as ttyio
-
 import bbsengine4 as bbsengine
 
 import libachilles
@@ -22,9 +21,53 @@ if loginid in membermap:
 else:
   memberid = None
 
+def add(args):
+  pass
+
 def main():
-    ttyio.echo("achilles")
-    return
+  parser = argparse.ArgumentParser(prog="achilles")
+  parser.add_argument("--verbose", default=True, action="store_true", help="use verbose mode")
+  parser.add_argument("--debug", default=False, action="store_true", help="run debug mode")
+  parser.add_argument("--dry-run", dest="dryrun", action="store_true", default=True, help="dry run (no database changes)")
+  # @todo: address 'zoidweb4' as hardcoded database name
+  bbsengine.buildargdatabasegroup(parser)
+  args = parser.parse_args()
+
+  mainmenu = (
+    ("L", "List"),
+    ("A", "Add"),
+    ("K", "Kill"),
+    ("E", "Edit")
+  )
+  done = False
+  while not done:
+    bbsengine.title("achilles - food item - terminal interface", hrcolor="{green}", titlecolor="{bggray}{white}")
+    buf = ""
+    for m in mainmenu:
+      buf += "{bgdarkgray}{white}[{yellow}%s{white}]{/all} -- %s{F6}" % (m[0], m[1])
+    buf += "{F6}{bgdarkgray}{white}[{yellow}Q{white}]{/all} -- Quit{F6}"
+    ttyio.echo(buf)
+    try:
+      ch = ttyio.inputchar("achilles main [LAKEQ]: {lightgreen}", "LAKEQ", "")
+    except (EOFError, KeyboardInterrupt) as e:
+      ttyio.echo("Quit")
+      return
+    finally:
+      pass
+      # ttyio.echo("{/all}")
+    if ch == "Q":
+      ttyio.echo("Quit")
+      done = True
+      continue
+    elif ch == "L":
+      ttyio.echo("List")
+    elif ch == "A":
+      ttyio.echo("Add")
+      add()
+    elif ch == "E":
+      ttyio.echo("Edit")
+      edit()
+  return
 
 if __name__ == "__main__":
     main()
