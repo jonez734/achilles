e5d4151a3226366f01d3f51948a7aab121363af0
Jeff MacDonald <jam@zoidtechnologies.com>
2021-09-22 22:12:10 -0400

    achilles/sql/seller.sql: removed commented out tables.

--- sql/seller.sql
+++ sql/seller.sql
@@ -8,9 +8,3 @@ create table achilles.__seller (
     "datemodified" timestamptz,
     "modifiedbyid" integer constraint fk_fooditem_modifiedbyid references engine.__member(id) on update cascade on delete set null
 );
-
---create table achilles.__store (
---);
-
---create table achilles.__restaraunt (
---);
