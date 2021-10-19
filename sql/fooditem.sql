7a82e49799783fcc792903d0bf771fab2a9a5dc1
Jeff MacDonald <jam@zoidtechnologies.com>
2021-10-19 19:16:51 -0400

    - converted to use engine.node - achilles.fooditem has a 'testedbyid' and 'datetested' - added ref to manuf.id - added 'labeladditives' and 'actualadditives' (arrays?) - 'price', 'datepurchased', and 'locationpurchased' have been moved to 'purchase'

--- sql/fooditem.sql
+++ sql/fooditem.sql
@@ -1,32 +1,52 @@
-create table achilles.__fooditem (
-  "id" serial unique not null primary key,
-  "upc" text unique,
-  "name" text,
-  "description" text,
-  "brand" text,
-  "qsr" boolean default 'f',
-  "frozen" boolean default 'f',
-  "price" numeric(10,2),
-  "msgpresent" boolean default 'f',
-  "msgonlabel" boolean default 'f',
-  "msgquantity" text,
-  "producturl" text,
-  "locationpurchased" text,
-  "datepurchased" timestamptz,
-  "postedbyid" integer constraint fk_fooditem_postedbyid references engine.__member(id) on update cascade on delete set null,
-  "dateposted" timestamptz,
-  "datemodified" timestamptz,
-  "modifiedbyid" integer constraint fk_fooditem_modifiedbyid references engine.__member(id) on update cascade on delete set null
-);
-
-grant insert, update, delete on achilles.__fooditem to "apache";
-
-create view achilles.fooditem as
-  select achilles.__fooditem.*,
-    extract(epoch from datepurchased) as datepurchasedepoch,
-    extract(epoch from dateposted) as datepostedepoch,
-    extract(epoch from datemodified) as datemodifiedepoch
-  from achilles.__fooditem
+create or replace view achilles.fooditem as
+  select n.*,
+    (n.attributes->>'upc')::text as upc,
+    (n.attributes->>'sku')::text as sku,
+    (n.attributes->>'name')::text as name,
+    (n.attributes->>'title')::text as title,
+    (n.attributes->>'manufid')::bigint as manufid,
+    (n.attributes->>'description')::text as description,
+    (n.attributes->>'labeladditives') as labeladditives,
+    (n.attributes->>'actualadditives') as actualadditives,
+    (n.attributes->>'testedbyid') as testedbyid,
+    (n.attributes->>'datetested') as datetested,
+    (n.attributes->>'frozen')::boolean as frozen
+  from engine.node as n
+  where (n.attributes->>'prg')::text = 'achilles.fooditem'
 ;
 
-grant select on achilles.fooditem to "apache";
+--create table achilles.__fooditem (
+--  "id" serial unique not null primary key,
+--  "upc" text unique,
+--  "name" text,
+--  "description" text,
+--  "brand" text,
+--  "industry" text, -- fast food (qsr), hospitality, etc
+--  "frozen" boolean default 'f',
+--  "price" numeric(10,2),
+--  "msgpresent" boolean default 'f',
+--  "msgonlabel" boolean default 'f',
+--  "dsgpresent" boolean default 'f',
+--  "dsgonlabel" boolean default 'f',
+--  "quantity" text,
+--  "producturl" text,
+--  "storeid" text,
+--  "locationpurchased" text,
+--  "datepurchased" timestamptz,
+--  "postedbyid" integer constraint fk_fooditem_postedbyid references engine.__member(id) on update cascade on delete set null,
+--  "dateposted" timestamptz,
+--  "datemodified" timestamptz,
+--  "modifiedbyid" integer constraint fk_fooditem_modifiedbyid references engine.__member(id) on update cascade on delete set null
+--);
+
+--grant insert, update, delete on achilles.__fooditem to "apache";
+
+--create view achilles.fooditem as
+--  select achilles.__fooditem.*,
+--    extract(epoch from datepurchased) as datepurchasedepoch,
+--    extract(epoch from dateposted) as datepostedepoch,
+--    extract(epoch from datemodified) as datemodifiedepoch
+--  from achilles.__fooditem
+--;
+
+--grant select on achilles.fooditem to "apache";
