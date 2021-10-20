17b16331527d7a4fe640ee40e01b5822c628c2a8
Jeff MacDonald <jam@zoidtechnologies.com>
2021-10-19 20:50:43 -0400

    - achilles/sql/retailer.sql: removed. see merchant.sql.

--- sql/retailer.sql
+++
@@ -1,9 +0,0 @@
-create view retailer as
-    select 
-        *,
-        (attributes->>'name')::text as name,
-        (attributes->>'qsr')::boolean as qsr,
-        (attributes->>'url')::text as url
-    from engine.node
-    where attributes ? 'name' and attributes ? 'qsr' and attributes ? 'url'
-;
