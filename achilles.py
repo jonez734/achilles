bee3bdb01b3f24fdae1ba9bbb8aae5c9b4d0819e
Jeff MacDonald <jam@zoidtechnologies.com>
2021-10-20 20:01:56 -0400

    - use modern try/except around call to main() for EOFError and KeyboardInterrupt, moved from main() - use bbsengine5-style for getting currentmemberid (database, not static table) - added code to add() and _edit() -

--- achilles.py
+++ achilles.py
@@ -1,5 +1,5 @@
 #
-# (C) 2020 Zoid Technologies. All Rights Reserved.
+# (C) 2021 zoidtechnologies.com All Rights Reserved.
 #
 
 import os
@@ -10,19 +10,62 @@ import argparse
 
 import getdate
 import ttyio4 as ttyio
-import bbsengine4 as bbsengine
+import bbsengine5 as bbsengine
 
 import libachilles
 
-membermap = {"jam" : 1}
-loginid = pwd.getpwuid(os.geteuid())[0]
-if loginid in membermap:
-  memberid = membermap[loginid]
-else:
-  memberid = None
+#membermap = {"jam" : 1}
+#loginid = pwd.getpwuid(os.geteuid())[0]
+#if loginid in membermap:
+#  memberid = membermap[loginid]
+#else:
+#  memberid = None
 
 def add(args):
-  pass
+  record = {}
+  record["title"] = ""
+  record["name"] = ""
+  
+  addrecord = _edit(args, record, "add")
+  
+  if record != addrecord:
+    ttyio.echo("** needs save **")
+  if ttyio.inputboolean("add fooditem? [yN]: ", "N") is True:
+    ttyio.echo("...code to add fooditem...")
+  return
+
+def _edit(args, record, mode="edit"):
+  editrecord = copy.deepcopy(record)
+
+  done = False
+  while not done:
+    ttyio.echo("[U]PC:        ")
+    ttyio.echo("[S]KU:        ")
+    ttyio.echo("[N]ame:       %s" % (editrecord["name"]), end="")
+    if record != editrecord:
+      ttyio.echo(" (was: %s)" % (record["name"]))
+    else:
+      ttyio.echo()
+    ttyio.echo("[T]itle:       %s" % (editrecord["title"]), end="")
+    if record != editrecord:
+      ttyio.echo(" (was: %s)" % (record["title"]))
+    else:
+      ttyio.echo()
+    ttyio.echo("[A]dditives:  ")
+    ttyio.echo("[F]rozen:     ")
+    ttyio.echo("[D]escription ")
+    ttyio.echo("[M]anuf:      ")
+    ch = ttyio.inputchar("%s fooditem [USNTADFM]: " % (mode), "USNTADFMQ", "Q")
+    if ch == "Q":
+      ttyio.echo("Quit")
+      break
+    elif ch == "N":
+      ttyio.echo("Name")
+      editrecord["name"] = ttyio.inputstring("fooditem name: ", editrecord["name"])
+    elif ch == "T":
+      ttyio.echo("Title")
+      editrecord["title"] = ttyio.inputstring("fooditem title: ", editrecord["title"])
+  return editrecord
 
 def main():
   parser = argparse.ArgumentParser(prog="achilles")
@@ -41,33 +84,37 @@ def main():
   )
   done = False
   while not done:
-    bbsengine.title("achilles - food item - terminal interface", hrcolor="{green}", titlecolor="{bggray}{white}")
+    bbsengine.title("achilles", hrcolor="{darkgreen}", titlecolor="{bggray}{white}")
     buf = ""
     for m in mainmenu:
       buf += "{bgdarkgray}{white}[{yellow}%s{white}]{/all} -- %s{F6}" % (m[0], m[1])
     buf += "{F6}{bgdarkgray}{white}[{yellow}Q{white}]{/all} -- Quit{F6}"
     ttyio.echo(buf)
-    try:
-      ch = ttyio.inputchar("achilles main [LAKEQ]: {lightgreen}", "LAKEQ", "")
-    except (EOFError, KeyboardInterrupt) as e:
-      ttyio.echo("Quit")
-      return
-    finally:
-      pass
-      # ttyio.echo("{/all}")
+#    try:
+    ch = ttyio.inputchar("achilles [LAKEQ]: {lightgreen}", "LAKEQ", "")
+#    except (EOFError, KeyboardInterrupt) as e:
+#      ttyio.echo("Quit{/all}")
+#      return
     if ch == "Q":
-      ttyio.echo("Quit")
+      ttyio.echo("Quit{/all}")
       done = True
       continue
     elif ch == "L":
-      ttyio.echo("List")
+      ttyio.echo("List{/all}")
     elif ch == "A":
-      ttyio.echo("Add")
-      add()
+      ttyio.echo("Add{/all}")
+      add(args)
     elif ch == "E":
-      ttyio.echo("Edit")
-      edit()
+      ttyio.echo("Edit{/all}")
+      edit(args)
   return
 
 if __name__ == "__main__":
+  try:
     main()
+  except EOFError:
+    ttyio.echo("{/all}{bold}EOF{/bold}")
+  except KeyboardInterrupt:
+    ttyio.echo("{/all}{bold}INTR{/bold}")
+  finally:
+    ttyio.echo("{/all}")
