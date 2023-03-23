a2736d5d419107514f525fec5b3fa0eaa5064554
Jeff MacDonald <jam@zoidtechnologies.com>
2023-03-23 19:29:22 -0400

    - achilles/www/config-prod.php: fixed up setting of includepath, changed DOCUMENTROOT to remove port number; updated SITEKEYWORDS, and changed project email address

--- www/config-prod.php
+++ www/config-prod.php
@@ -1,19 +1,19 @@
 <?php
 
-define("SITEADMINEMAIL", "jam <jam@zoidtechnologies.com>");
+define("SITEADMINEMAIL", "achilles <achilles@projects.zoidtechnologies.com>");
 
 /**
  * define the base url for the site. THIS VALUE MUST BE TERMINATED WITH A "/"
  */
 define("SITETITLE", "Project Achilles - What is the answer to life, the universe, and everything medicine?");
 define("SITEURL", "http://achilles.zoidtechnologies.com/");
-define("SITEKEYWORDS", "project achilles, monosodium glutamate, mono-sodium glutamate, msg, glutamate, truth in labeling, health, nutrition, consumer protection, food additives");
+define("SITEKEYWORDS", "project achilles, monosodium glutamate, mono-sodium glutamate, msg, glutamate, disodium glutamate, truth in labeling, health, nutrition, consumer protection, food additives");
 define("SITEDESCRIPTION", "Project Achilles studies the effects of monosodium glutamate on the human body");
 define("SITETYPE", "website");
 define("SKINURL", SITEURL . "skin/");
 //define("SYSTEMDSN", "pgsql://apache@127.0.0.1/zoidweb3");
 define("VHOSTDIR", "/srv/www/vhosts/achilles.zoidtechnologies.com/");
-define("DOCUMENTROOT", VHOSTDIR . "80/html/");
+define("DOCUMENTROOT", VHOSTDIR . "html/");
 
 define("IMAGESURL", "http://static.zoidtechnologies.com/");
 
@@ -24,12 +24,14 @@ define("SMARTYTEMPLATESDIR", VHOSTDIR."tmpl/");
 define("LOGENTRYPREFIX", "achillesprod");
 
 /*
- * @since 20191024
+ * @since 20190817
+ * @since 20221104
  */
-$includepath = get_include_path();
-$zoidweb4 = str_replace("zoidweb3", "zoidweb4", $includepath);
-set_include_path($zoidweb4);
-
+$includepath = get_include_path().":/srv/www/zoidweb4/php:/srv/www/bbsengine4/php";
+if (set_include_path($includepath) === false)
+{
+    print("include path fail");
+}
 /**
  * @since 20200422
  * 3
