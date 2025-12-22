893341d9f699fee028ccadc14052a973eeaade19
Jeff MacDonald <jam@zoidtechnologies.com>
2025-12-22 17:49:17 -0500

    achilles: added section.tmpl and section.js

--- www/skin/js/section.js
+++ www/skin/js/section.js
@@ -1,26 +1,29 @@
-// skin/js/achilles-sections.js
 $(document).ready(function() {
     $('.section-header').on('click', function() {
         var $section = $(this).closest('.section');
         var $content = $section.find('.section-content');
         var sectionName = $section.data('name');
 
-        // Toggle visibility with a slide effect
-        $content.slideToggle();
-
-        // Only fetch if the container is currently empty
-        if ($content.is(':empty')) {
-            $content.html('<div class="loading">Loading...</div>');
+        // CASE 1: Content is already there (either open or previously loaded)
+        if ($.trim($content.html()) !== '') {
+            console.log("Toggling existing content for: " + sectionName);
+            $content.slideToggle(); // This handles the "close" if it's currently open
+        } 
+        // CASE 2: Content is empty (first time click)
+        else {
+            console.log("First load for: " + sectionName);
             
             $.ajax({
-                url: 'section.php',
+                url: '/achilles/section.php',
                 type: 'GET',
                 data: { name: sectionName },
                 success: function(data) {
                     $content.html(data);
+                    // Hide immediately so slideDown has a starting point of 0 height
+                    $content.hide().slideDown(); 
                 },
                 error: function() {
-                    $content.html('<div class="error">Failed to load content.</div>');
+                    $content.html('<div class="error">Error loading content.</div>').slideDown();
                 }
             });
         }
