d957537f620a2fefd73c509ccfa4a275f7855b19
J (eff) <jam@zoidtechnologies.com>
2026-03-31 13:03:35 -0400

    Fix: Define SMARTYTEMPLATESDIR after SKINDIR constant
    SMARTYTEMPLATESDIR was previously defined in zoid6config.php before SKINDIR was
    available, causing fatal 'Undefined constant' errors.
    
    This moves the definition to config-prod.php after SKINDIR is defined, ensuring
    proper constant dependency ordering.

--- www/config-prod.php
+++ www/config-prod.php
@@ -3,6 +3,7 @@
 require_once('/srv/www/bbsengine6/php/bootstrap.php');
 require_once('util.php');
 \bbsengine6\util\add_include_paths(['/srv/www/zoid6/php/', '/srv/www/bbsengine6/php/', '/srv/www/smarty/']);
+require_once('zoid6config.php');
 
 // Define configuration constants in config namespace (required by bbsengine6)
 // Using backslash prefix makes them accessible via defined('\config\CONSTANT')
@@ -16,9 +17,17 @@ define("config\DOCUMENTROOT", \config\VHOSTDIR . "html/achilles/");
 define("config\SKINDIR", \config\DOCUMENTROOT . "skin/");
 define("config\SKINURL", \config\SITEURL . "skin/");
 define("config\JSURL", "/achilles/skin/js/");
-define("config\SMARTYCOMPILEDTEMPLATESDIR", \config\VHOSTDIR . "templates_c");
-define("config\SMARTYPLUGINSDIR", [ 0 => \config\VHOSTDIR . "smarty/", 1 => "/srv/www/zoid6/smarty/"]);
-define("config\SMARTYTEMPLATESDIR", [ 0 => \config\SKINDIR . "tmpl/", 1=> "/srv/www/zoid6/skin/tmpl/", 2 => "/srv/www/bbsengine6/skin/tmpl/"]);
+
+// SMARTYTEMPLATESDIR - 3-element array with proper precedence
+// Search order: 1) Site-specific templates 2) bbsengine6 shared 3) zoid6 shared
+if (!defined("config\SMARTYTEMPLATESDIR")) {
+    define("config\SMARTYTEMPLATESDIR", [
+        0 => \config\SKINDIR . "tmpl/",
+        1 => "/srv/www/bbsengine6/skin/tmpl/",
+        2 => "/srv/www/zoid6/skin/tmpl/"
+    ]);
+}
+
 define("config\LOGENTRYPREFIX", "zoid6achilles");
 define("config\ENGINEURL", "/engine/");
 define("config\GOOGLEANALYTICSACCOUNT", "UA-23705021-1");
@@ -32,9 +41,6 @@ define("SKINDIR", \config\SKINDIR);
 define("SKINURL", \config\SKINURL);
 define("JSURL", \config\JSURL);
 define("ENGINEURL", \config\ENGINEURL);
-define("SMARTYTEMPLATESDIR", \config\SMARTYTEMPLATESDIR);
-define("SMARTYCOMPILEDTEMPLATESDIR", \config\SMARTYCOMPILEDTEMPLATESDIR);
-define("SMARTYPLUGINSDIR", \config\SMARTYPLUGINSDIR);
 define("LOGENTRYPREFIX", \config\LOGENTRYPREFIX);
 define("SECTIONTEMPLATEDIR", \config\SECTIONTEMPLATEDIR);
 
