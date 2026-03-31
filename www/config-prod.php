0c6a892fb137578535065549d37d064adebd39b6
J (eff) <jam@zoidtechnologies.com>
2026-03-30 21:38:41 -0400

    Fix achilles config namespace and add global aliases
    Apply same fix as zoid6/sites/www:
    - Move namespace statement to top (PHP requirement)
    - Define all constants using config\ namespace prefix
    - Add global aliases for application code compatibility
    - Remove problematic namespace block structure
    
    This fixes the 'index.tmpl not found' error by ensuring:
    1. bbsengine6's getsmarty() can find config\SMARTYTEMPLATESDIR
    2. Application code can access constants directly (SMARTYTEMPLATESDIR)
    3. All path construction works correctly within namespace

--- www/config-prod.php
+++ www/config-prod.php
@@ -1,38 +1,41 @@
 <?php
 
-define("SITEADMINEMAIL", "achilles <achilles@projects.zoidtechnologies.com>");
+require_once('/srv/www/bbsengine6/php/bootstrap.php');
+require_once('util.php');
+\bbsengine6\util\add_include_paths(['/srv/www/zoid6/php/', '/srv/www/bbsengine6/php/', '/srv/www/smarty/']);
 
-define("SITETITLE", "Project Achilles - A study of the affects of food additives in the diet");
-define("SITEURL", "http://achilles.zoidtechnologies.com/");
-define("SITEKEYWORDS", "project achilles, monosodium glutamate, mono-sodium glutamate, msg, glutamate, disodium glutamate, truth in labeling, health, nutrition, consumer protection, food additives");
-define("SITEDESCRIPTION", "Project Achilles studies the effects of monosodium glutamate on the human body");
-define("SITETYPE", "website");
-define("SKINURL", SITEURL . "skin/");
-//define("SYSTEMDSN", "pgsql://apache@127.0.0.1/zoidweb3");
-define("VHOSTDIR", "/srv/www/vhosts/achilles.zoidtechnologies.com/");
-define("DOCUMENTROOT", VHOSTDIR . "html/");
+// Define configuration constants in config namespace (required by bbsengine6)
+// Using backslash prefix makes them accessible via defined('\config\CONSTANT')
+define("config\SITEADMINEMAIL", "achilles <achilles@projects.zoidtechnologies.com>");
+define("config\SITETITLE", "Project Achilles - A study of the affects of flavor enhancers on health");
+define("config\SITEURL", "/achilles/");
+define("config\SITEKEYWORDS", "project achilles, monosodium glutamate, mono-sodium glutamate, msg, glutamate, disodium glutamate, truth in labeling, health, nutrition, consumer protection, food additives");
+define("config\SITEDESCRIPTION", "Project Achilles studies the affects of flavor enhancers on health");
+define("config\VHOSTDIR", "/srv/www/vhosts/zoidtechnologies.com/");
+define("config\DOCUMENTROOT", \config\VHOSTDIR . "html/achilles/");
+define("config\SKINDIR", \config\DOCUMENTROOT . "skin/");
+define("config\SKINURL", \config\SITEURL . "skin/");
+define("config\JSURL", "/achilles/skin/js/");
+define("config\SMARTYCOMPILEDTEMPLATESDIR", \config\VHOSTDIR . "templates_c");
+define("config\SMARTYPLUGINSDIR", [ 0 => \config\VHOSTDIR . "smarty/", 1 => "/srv/www/zoid6/smarty/"]);
+define("config\SMARTYTEMPLATESDIR", [ 0 => \config\SKINDIR . "tmpl/", 1=> "/srv/www/zoid6/skin/tmpl/", 2 => "/srv/www/bbsengine6/skin/tmpl/"]);
+define("config\LOGENTRYPREFIX", "zoid6achilles");
+define("config\ENGINEURL", "/engine/");
+define("config\GOOGLEANALYTICSACCOUNT", "UA-23705021-1");
+define("config\SECTIONTEMPLATEDIR", \config\SKINDIR . "tmpl/sections/");
 
-define("IMAGESURL", "http://static.zoidtechnologies.com/");
-
-define("SMARTYCOMPILEDTEMPLATESDIR", VHOSTDIR . "templates_c");
-define("SMARTYPLUGINSDIR", VHOSTDIR . "smarty/");
-define("SMARTYTEMPLATESDIR", VHOSTDIR."tmpl/");
-
-define("LOGENTRYPREFIX", "achillesprod");
-
-/*
- * @since 20190817
- * @since 20221104
- */
-$includepath = get_include_path().":/srv/www/zoidweb4/php:/srv/www/bbsengine4/php";
-if (set_include_path($includepath) === false)
-{
-    print("include path fail");
-}
-/**
- * @since 20200422
- * 3
- */
-define("GOOGLEANALYTICSACCOUNT", "UA-23705021-1");
+// Create global aliases for code that references constants directly without namespace
+define("SITEURL", \config\SITEURL);
+define("VHOSTDIR", \config\VHOSTDIR);
+define("DOCUMENTROOT", \config\DOCUMENTROOT);
+define("SKINDIR", \config\SKINDIR);
+define("SKINURL", \config\SKINURL);
+define("JSURL", \config\JSURL);
+define("ENGINEURL", \config\ENGINEURL);
+define("SMARTYTEMPLATESDIR", \config\SMARTYTEMPLATESDIR);
+define("SMARTYCOMPILEDTEMPLATESDIR", \config\SMARTYCOMPILEDTEMPLATESDIR);
+define("SMARTYPLUGINSDIR", \config\SMARTYPLUGINSDIR);
+define("LOGENTRYPREFIX", \config\LOGENTRYPREFIX);
+define("SECTIONTEMPLATEDIR", \config\SECTIONTEMPLATEDIR);
 
 ?>
